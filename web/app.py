972ff48d1381cb445d366f0c1e47bbdc54958a13
kbartosz12 <krzysztofbartosz12@gmail.com>
2018-05-26 11:15:15 +0200

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,20 +1,27 @@
 from flask import Flask
 from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "It works Hurray!!!!"
+  return "It works!!"
 
 @app.route("/order-animation")
 def order_animation():
   return render_template(
-    "order.html",
-    invitation="Welcome at ani creatior"
-  )
+  "order.html",
+  invitation="Welcome to ani creator")
 
+@app.route('/Upload')
+def upload():
+  if 'my_file' not in request.files:
+    return "something is not yes"
+  upl_file = request.files['my_file']
+  storage - new S3Storage(os.get, s3)
+  storage.save(path="", body=)
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port=8080, debug=True)
+  app.run(host="0.0.0.0", port=8080, debug=True )
 
 
